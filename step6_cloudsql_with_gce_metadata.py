96316339d340b9602e91e28e13033da68997f680
Your Name <you@example.com>
2020-12-28 18:02:01 +0000

    first commit

--- step6_cloudsql_with_gce_metadata.py
+++ step6_cloudsql_with_gce_metadata.py
@@ -1,14 +1,17 @@
-#
 # curl http://169.254.169.254/computeMetadata/v1/instance/attributes/ -H "Metadata-Flavor: Google"
 
+# 透過metadata來取得db的相關資訊，並以CloudSQL 提供的PrivateIP進行連線
 import requests
 
+# 客製化元數組 - db_host
 db_host_res=requests.get('http://169.254.169.254/computeMetadata/v1/instance/attributes/db_host', headers = {'Metadata-Flavor': 'Google'})
 db_host=db_host_res.text
 
+# 客製化元數組 - db_user
 db_user_res=requests.get('http://169.254.169.254/computeMetadata/v1/instance/attributes/db_user', headers = {'Metadata-Flavor': 'Google'})
 db_user=db_user_res.text
 
+# 客製化元數組 - db_password
 db_password_res=requests.get('http://169.254.169.254/computeMetadata/v1/instance/attributes/db_password', headers = {'Metadata-Flavor': 'Google'})
 db_password=db_password_res.text
 
@@ -18,7 +21,6 @@ import pymysql
 connection = pymysql.connect(host=db_host,
                              user=db_user,
                              password=db_password)
-
 
 # 查詢有多少databases
 some_sql_sentences= connection.cursor()
